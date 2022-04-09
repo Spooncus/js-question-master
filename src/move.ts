390e30d427e52b56f4567463e7981f40615e4a19
Doğukan Kırali <yaaklimabisitakildi@gmail.com>
2022-04-09 15:16:33 +0300

    For-Of Linting

--- src/move.ts
+++ src/move.ts
@@ -5,52 +5,52 @@ type Folder = {
   id: string;
   name: string;
   files: File[];
-}
+};
 
 type File = {
   id: string;
   name: string;
-}
+};
 
 export default function move(list: List, source: string, destination: string): List {
+  let fileFound = false;
+  let destinationFound = false;
+  let destinationIndex = 0;
+  let sourceFile: File = {
+    id: '',
+    name: '',
+  };
 
-  let file_found: boolean = false
-  let destination_found: boolean = false
-  let destination_index: number = 0
-  let source_file: File | undefined = undefined
-
-  for(let folder of list) {
+  for (const folder of list) {
     // ilk dosta hedef dosya olabilir bu yüzden indexi bulmak istiyoruz
     if (folder.id === destination) {
-      destination_index = list.indexOf(folder) 
-      destination_found = true
+      destinationIndex = list.indexOf(folder);
+      destinationFound = true;
     }
-
     // source dosyayı elde ediyoruz ve kaynak klasörden siliyoruz
-    for(let file of folder.files) {
-      if(file.id === source) {
-        source_file = file
-        folder.files = folder.files.filter(oneFile => oneFile.id !== file.id)
-        file_found = true
-        break
+    for (const file of folder.files) {
+      if (file.id === source) {
+        sourceFile = file;
+        const index = folder.files.indexOf(file, 0);
+        if (index > -1) {
+          folder.files.splice(index, 1);
+        }
+        fileFound = true;
+        break;
       }
     }
-    
     // 100 dosyalı bir sistemde destionation 1. index, source 2. indexteyse tüm foru dönmesine gerek kalmasın
-    if (file_found && destination_found) {
-      break
+    if (fileFound && destinationFound) {
+      break;
     }
   }
 
-  if(file_found === false) {
+  if (fileFound === false) {
     throw new Error('You cannot move a folder');
-  } else if(destination_found === false) {
+  } else if (destinationFound === false) {
     throw new Error('You cannot specify a file as the destination');
   }
 
-  if(source_file !== undefined) {
-    list[destination_index].files.push(source_file);
-  }
-  
+  list[destinationIndex].files.push(sourceFile);
   return list;
 }
